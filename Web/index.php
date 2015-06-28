cf9db861472ad80df4aabbb28717c94e49cd0f65
walkor <walkor@workerman.net>
2015-06-29 00:07:21 +0800

    save

--- Web/index.php
+++ Web/index.php
@@ -72,6 +72,7 @@
       <a href="https://github.com/walkor/Workerman/archive/v3.1.7.zip" class="btn btn-outline-inverse btn-lg">Download Workerman</a>
     </p>
     <p class="version">Currently v3.1.7</p>
+    <br>
   </div>
 </main>
 
@@ -84,6 +85,7 @@
 
        <h2 class="bs-docs-featurette-title">Examples</h2>
        <div class="col-sm-6">
+         <h3>WebSocket Server</h3>
          <div style="text-align:left;background-color:#EEEEEE;padding:15px;font-size:13px;">
          <?php 
           $codes = '<?php
@@ -124,6 +126,7 @@ echo highlight_string($codes,true);
     </div>
 
     <div class="col-sm-6">
+      <h3>Http Server</h3>
       <div style="text-align:left;background-color:#EEEEEE;padding:15px;font-size:13px;margin:8px;">
       <?php
        $codes = '<?php
@@ -131,12 +134,17 @@ require_once \'./Workerman/Autoloader.php\';
 use Workerman\Worker;
 
 // #### http worker ####
-$http_worker = new Worker("http://0.0.0.0:2345");
+$http_worker = new Worker("http://0.0.0.0:80");
+
+// 4 processes
 $http_worker->count = 4;
+
+// Emitted when request received
 $http_worker->onMessage = function($connection, $data)
 {
     // $_GET, $_POST, $_COOKIE, $_SESSION, $_SERVER, $_FILES are available
     var_dump($_GET, $_POST, $_COOKIE, $_SESSION, $_SERVER, $_FILES);
+
     // send data to client
     $connection->send("hello world \n");
 };
@@ -154,45 +162,8 @@ echo highlight_string($codes,true);
 
 <div class="bs-docs-featurette">
   <div class="container">
-    <p class="lead">Bootstrap is open source. It's hosted, developed, and maintained on GitHub.</p>
+    <p class="lead">Workerman is open source. It's hosted, developed, and maintained on GitHub.</p>
     <a href="https://github.com/walkor/workerman" class="btn btn-outline btn-lg">View the GitHub project</a>
-    <h2 class="bs-docs-featurette-title">Built with Bootstrap.</h2>
-    <p class="lead">Millions of amazing sites across the web are being built with Bootstrap. Get started on your own with our growing <a href="http://getbootstrap.com/getting-started/#examples">collection of examples</a> or by exploring some of our favorites.</p>
-
-    <hr class="half-rule">
-
-    <div class="row bs-docs-featured-sites">
-    
-      <div class="col-xs-6 col-sm-3">
-        <a href="http://expo.getbootstrap.com/2014/10/29/lyft/" target="_blank" title="Lyft">
-          <img src="/img/browserquest.jpg" alt="Lyft" class="img-responsive">
-        </a>
-      </div>
-    
-      <div class="col-xs-6 col-sm-3">
-        <a href="http://expo.getbootstrap.com/2014/09/30/vogue/" target="_blank" title="Vogue">
-          <img src="/img/shadowsocks-php.png" alt="Vogue" class="img-responsive">
-        </a>
-      </div>
-    
-      <div class="col-xs-6 col-sm-3">
-        <a href="http://expo.getbootstrap.com/2014/03/13/riot-design/" target="_blank" title="Riot Design">
-          <img src="/img/web-msg-sender.png" alt="Riot Design" class="img-responsive">
-        </a>
-      </div>
-    
-      <div class="col-xs-6 col-sm-3">
-        <a href="http://expo.getbootstrap.com/2014/02/12/newsweek/" target="_blank" title="Newsweek">
-          <img src="/img/workerman-todpole.png" alt="Newsweek" class="img-responsive">
-        </a>
-      </div>
-    
-    </div>
-
-    <hr class="half-rule">
-
-    <p class="lead">We showcase dozens of inspiring projects built with Bootstrap on the Bootstrap Expo.</p>
-    <a href="http://expo.getbootstrap.com/" class="btn btn-outline btn-lg">Explore the Expo</a>
   </div>
 </div>
 
@@ -201,41 +172,21 @@ echo highlight_string($codes,true);
 ================================================== -->
 <footer class="bs-docs-footer" role="contentinfo">
   <div class="container">
-    <p>Designed and built with all the love in the world by <a href="https://twitter.com/mdo" target="_blank">@mdo</a> and <a href="https://twitter.com/fat" target="_blank">@fat</a>.</p>
-    <p>Maintained by the <a href="https://github.com/orgs/twbs/people">core team</a> with the help of <a href="https://github.com/twbs/bootstrap/graphs/contributors">our contributors</a>.</p>
-    <p>Code licensed under <a rel="license" href="https://github.com/twbs/bootstrap/blob/master/LICENSE" target="_blank">MIT</a>, documentation under <a rel="license" href="https://creativecommons.org/licenses/by/3.0/" target="_blank">CC BY 3.0</a>.</p>
+    <p>Code licensed under <a rel="license" href="https://github.com/walkor/Workerman/blob/master/MIT-LICENSE.txt" target="_blank">MIT</a></p>
     <ul class="bs-docs-footer-links text-muted">
-      <li>Currently v3.3.5</li>
+      <li>Currently v3.1.7</li>
       <li>·</li>
-      <li><a href="https://github.com/twbs/bootstrap">GitHub</a></li>
+      <li><a href="https://github.com/walkor/workerman">GitHub</a></li>
       <li>·</li>
-      <li><a href="http://getbootstrap.com/getting-started/#examples">Examples</a></li>
+      <li><a href="http://wiki.workerman.net">Documentation</a></li>
       <li>·</li>
-      <li><a href="http://getbootstrap.com/2.3.2/">v2.3.2 docs</a></li>
+      <li><a href="https://github.com/walkor/workerman/issues">Issues</a></li>
       <li>·</li>
-      <li><a href="http://getbootstrap.com/about/">About</a></li>
-      <li>·</li>
-      <li><a href="http://expo.getbootstrap.com/">Expo</a></li>
-      <li>·</li>
-      <li><a href="http://blog.getbootstrap.com/">Blog</a></li>
-      <li>·</li>
-      <li><a href="https://github.com/twbs/bootstrap/issues">Issues</a></li>
-      <li>·</li>
-      <li><a href="https://github.com/twbs/bootstrap/releases">Releases</a></li>
+      <li><a href="https://github.com/walkor/workerman/releases">Releases</a></li>
     </ul>
   </div>
 </footer>
-
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
 <script src="/js/jquery.min.js"></script>
-
-
 <script src="/js/bootstrap.min.js"></script>
-
-
-
 <script src="/js/style.js"></script>
-
 </body></html>
